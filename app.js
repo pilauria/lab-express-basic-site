17d4a26e1c34a50953c2163d6401320fe2e9ef3c
pilauria <ptr.lauria@gmail.com>
2021-12-03 14:21:18 +0100

    Final

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
+// Require Express
 const express = require('express');
 
 const path = require('path');
-
+// Express server handling requests and responses
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'views')));
+// // Make everything inside of views/ available
+// app.use(express.static(path.join(__dirname, 'views')));
+
+// Make everything inside of public/ available (default folder)
 app.use(express.static(path.join(__dirname, 'public')));
 
+// '/' route:
 app.all('/', (request, response) => {
   response.sendFile(path.join(__dirname, 'views', 'home.html'));
+  // console.log(__dirname); // to see the complete path to the HTML file
 });
-
+// '/about' route:
 app.all('/about', (request, response) => {
   response.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
-
+// '/works' route:
 app.all('/works', (request, response) => {
   response.sendFile(path.join(__dirname, 'views', 'works.html'));
 });
-
+// '/photogallery' route:
 app.all('/photogallery', (request, response) => {
   response.sendFile(path.join(__dirname, 'views', 'photogallery.html'));
 });
 
+// start app
 app.listen(3000, () => {
   console.log('Yey APP running');
 });
+
+// npx kill-port 3000
